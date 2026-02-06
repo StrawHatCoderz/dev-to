59eac0b345f29af0a015a66f4fc6f9bcc246ae2f
Jyothi Praveen Gorre <jyothipraveengorre@Jyothis-MacBook-Pro.local>
2026-02-06 11:48:50 +0530

    starts implementing the get stories handler
    * checking whether the id exist in the session

--- server/src/user/stories.js
+++ server/src/user/stories.js
@@ -1,1 +1,17 @@
-console.log("gorre j p");
+const isIdExistInTheSession = (id, session) => {
+  return id in session;
+}
+
+export const getStoriesHandler = (id, session, users) => {
+  if (isIdExistInTheSession(id, session)) {
+    return { error: true, errMsg: "id does not exist in the session" };
+  }
+};
+
+/*
+
+request contain the user id
+check the id is present in session
+if not send id not exist in the session
+if it is there then send the stories as response
+*/
---
+++ server/test/get_stories_test.js
@@ -0,0 +1,11 @@
+import { assertEquals } from "@std/assert";
+import { describe, it } from "jsr:@std/testing/bdd";
+import { getStoriesHandler } from "../src/user/stories.js";
+
+// describe("testing the get stories functionality : ", () => {
+//   it("providing an user id , where user does not exist in the session: ", () => {
+//     const actual = getStoriesHandler(1, { id: 2 }, []);
+//     const expected  = true;
+//     // assertEquals(actual, undefined)
+//   });
+// });
