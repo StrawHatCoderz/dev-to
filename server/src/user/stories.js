2428c88462ae68f078bb5b208d886e31c50c8c92
Jyothi Praveen Gorre <jyothipraveengorre@Jyothis-MacBook-Pro.local>
2026-02-06 11:55:10 +0530

    get stories function now delegates to handle error ,if id does not exist in session
    * which returns the error status as true

--- server/src/user/stories.js
+++ server/src/user/stories.js
@@ -1,11 +1,12 @@
-const isIdExistInTheSession = (id, session) => {
-  return id in session;
-}
+const isIdExistInTheSession = (id, users) => {
+  return users.includes(id);
+};
 
 export const getStoriesHandler = (id, session, users) => {
-  if (isIdExistInTheSession(id, session)) {
+  if (!isIdExistInTheSession(id, session.users)) {
     return { error: true, errMsg: "id does not exist in the session" };
   }
+  return { error : false}
 };
 
 /*
@@ -14,4 +15,7 @@ request contain the user id
 check the id is present in session
 if not send id not exist in the session
 if it is there then send the stories as response
+
+session : {
+users : []}
 */
