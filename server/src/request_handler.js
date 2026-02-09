7fe9dfcea0db3e1b4f34fa3f9647aa07e531da23
Chiranjeevi Gurram <chiranjeevigurram@Chiranjeevis-MacBook-Pro.local>
2026-02-09 18:44:05 +0530

    [chiru][handle common requests] : corrects spelling mistake of function name

--- server/src/request_handler.js
+++ server/src/request_handler.js
@@ -1,40 +1,40 @@
-import { handleCommonReqests } from './routes/handle_common_requests.js';
+import { handleCommonRequests } from "./routes/handle_common_requests.js";
 
 export const parseGetRequest = (path) => {
-	const [_, route, ...params] = path.split('/');
-	return { route, params };
+  const [_, route, ...params] = path.split("/");
+  return { route, params };
 };
 
 export const parsePostRequest = async (path, request) => {
-	const [_, route] = path.split('/');
-	const body = await request.json();
-	return { route, body };
+  const [_, route] = path.split("/");
+  const body = await request.json();
+  return { route, body };
 };
 
 const methodHandler = {
-	GET: parseGetRequest,
-	POST: parsePostRequest,
+  GET: parseGetRequest,
+  POST: parsePostRequest,
 };
 
 export const parseRequest = async (request) => {
-	const pathname = new URL(request.url).pathname;
-	const parser = methodHandler[request.method];
-	return await parser(pathname, request);
+  const pathname = new URL(request.url).pathname;
+  const parser = methodHandler[request.method];
+  return await parser(pathname, request);
 };
 
 export const requestHandler = async (request) => {
-	const parsedRequest = await parseRequest(request);
-	const pathname = new URL(request.url).pathname;
-	const prefix = pathname.split('/')[0] || '/';
+  const parsedRequest = await parseRequest(request);
+  const pathname = new URL(request.url).pathname;
+  const prefix = pathname.split("/")[0] || "/";
 
-	const handlers = {
-		'/': handleCommonReqests,
-	};
+  const handlers = {
+    "/": handleCommonRequests,
+  };
 
-	const handler = handlers[prefix];
-	if (!handler) return new Response('Invalid Route');
+  const handler = handlers[prefix];
+  if (!handler) return new Response("Invalid Route");
 
-	const response = handler(parsedRequest);
-	console.log(response);
-	return new Response(response);
+  const response = handler(parsedRequest);
+  console.log(response);
+  return new Response(response);
 };
--- server/test/handle_common_requests_test.js
+++ server/test/handle_common_requests_test.js
@@ -1,47 +1,47 @@
 import { describe, it } from "@std/testing/bdd";
 import { assertEquals } from "@std/assert";
-import { handleCommonReqests } from "../src/routes/handle_common_requests.js";
+import { handleCommonRequests } from "../src/routes/handle_common_requests.js";
 import { session } from "../src/mock/current-session.js";
 
 describe("handle common requests", () => {
   it("=> login should return success response when user have acc", () => {
     const requestInfo = { route: "login", body: "deadpool" };
-    const response = handleCommonReqests(requestInfo);
+    const response = handleCommonRequests(requestInfo);
     assertEquals(response.success, true);
     assertEquals(response.status, 200);
     assertEquals(session.users, [1]);
   });
   it("=> login should return failure response when user doesn't have acc", () => {
     const requestInfo = { route: "login", body: "dksl" };
-    const response = handleCommonReqests(requestInfo);
+    const response = handleCommonRequests(requestInfo);
     assertEquals(response.success, false);
     assertEquals(response.status, 401);
     assertEquals(session.users, [1]);
   });
   it("=> logout should return success response", () => {
     const requestInfo = { route: "logout", body: 1 };
-    const response = handleCommonReqests(requestInfo);
+    const response = handleCommonRequests(requestInfo);
     assertEquals(response.success, true);
     assertEquals(response.status, 200);
     assertEquals(session.users, []);
   });
   it("=> logout should return failure response", () => {
     const requestInfo = { route: "logout", body: 2 };
-    const response = handleCommonReqests(requestInfo);
+    const response = handleCommonRequests(requestInfo);
     assertEquals(response.success, false);
     assertEquals(response.status, 401);
     assertEquals(session.users, []);
-  })
+  });
   it("=> get every story ", () => {
     const requestInfo = { route: "stories" };
-    const response = handleCommonReqests(requestInfo);
+    const response = handleCommonRequests(requestInfo);
     assertEquals(response.success, true);
     assertEquals(response.stories, [{
-	id : 1,
-	authorId: 1,
-	title: "abc",
-	content: "abc",
-	isPublished: true
-}])
-  })
+      id: 1,
+      authorId: 1,
+      title: "abc",
+      content: "abc",
+      isPublished: true,
+    }]);
+  });
 });
