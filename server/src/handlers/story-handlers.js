6f65384016f9ffc6e34d9251c4bb566fd3161811
Umar Khaji <umarkhaji@Umars-MacBook-Pro.local>
2026-02-09 11:06:06 +0530

    [umar] moves follow, unfollow, createStory to their own handler files

--- server/src/handlers/story-handlers.js
+++ server/src/handlers/story-handlers.js
@@ -1,3 +1,42 @@
+const getClapIndex = (story, userId) =>
+	story.claps.findIndex((clap) => clap.clappedBy === userId);
+
+const isAlreadyClapped = (story, userId) => {
+	return getClapIndex(story, userId) !== -1;
+};
+
+const unClapStory = (_id, story, userId, _storyId) => {
+	const clapIndex = getClapIndex(story, userId);
+	story.claps.splice(clapIndex, 1);
+};
+
+const clapStory = (id, story, clappedBy, storyId) => {
+	const clap = { id, storyId, clappedBy };
+	story.claps.push(clap);
+};
+
+const doesStoryExists = (storyId, stories) => {
+	return stories.some((story) => story.id === storyId);
+};
+
+const isValidContent = (story) => {
+	return story.content.length !== 0;
+};
+
+export const toggleClap = (userId, storyId, stories) => {
+	const story = stories.find((story) => story.id === storyId);
+
+	if (!story) {
+		return { success: false, status: 404 };
+	}
+
+	const action = isAlreadyClapped(story, userId) ? unClapStory : clapStory;
+	const clapId = story.claps.length;
+	action(clapId, story, userId, storyId);
+
+	return { success: true, status: 200 };
+};
+
 export const addComment = (content, storyId, comments, userId) => {
 	if (!content) {
 		return { success: false, status: 400 };
@@ -26,45 +65,6 @@ export const getComments = (storyId, comments) => {
 	return { success: true, status: 200, comments: storyComments };
 };
 
-const getClapIndex = (story, userId) =>
-	story.claps.findIndex((clap) => clap.clappedBy === userId);
-
-const isAlreadyClapped = (story, userId) => {
-	return getClapIndex(story, userId) !== -1;
-};
-
-const unClapStory = (_id, story, userId, _storyId) => {
-	const clapIndex = getClapIndex(story, userId);
-	story.claps.splice(clapIndex, 1);
-};
-
-const clapStory = (id, story, clappedBy, storyId) => {
-	const clap = { id, storyId, clappedBy };
-	story.claps.push(clap);
-};
-
-export const toggleClap = (userId, storyId, stories) => {
-	const story = stories.find((story) => story.id === storyId);
-
-	if (!story) {
-		return { success: false, status: 404 };
-	}
-
-	const action = isAlreadyClapped(story, userId) ? unClapStory : clapStory;
-	const clapId = story.claps.length;
-	action(clapId, story, userId, storyId);
-
-	return { success: true, status: 200 };
-};
-
-const doesStoryExists = (storyId, stories) => {
-	return stories.some((story) => story.id === storyId);
-};
-
-const isValidContent = (story) => {
-	return story.content.length !== 0;
-};
-
 export const getStory = (storyId, stories) => {
 	if (!isValidContent(story)) {
 		return { success: true, status: 400 };
@@ -76,4 +76,46 @@ export const getStory = (storyId, stories) => {
 
 	const story = stories.find((story) => story.id === id);
 	return { success: true, status: 200, story };
+};
+
+const addStory = (id, { title, content, authorId }, stories) => {
+	const story = {
+		id,
+		authorId,
+		title,
+		content,
+		claps: [],
+		comments: [],
+	};
+
+	stories.push(story);
+};
+
+export const createStoryHandler = (storyToCreate, stories) => {
+	const { title, content } = storyToCreate;
+	const isValidContent = ![content.trim().length, title.trim().length].includes(
+		0,
+	);
+
+	if (!isValidContent) {
+		return { success: false, status: 400 };
+	}
+
+	const id = stories.length + 1;
+	addStory(id, storyToCreate, stories);
+
+	return { success: true, status: 200 };
+};
+
+export const deleteStory = (storyId, stories) => {
+	const storyIndex = stories.findIndex(
+		({ storyId: currentStoryId }) => currentStoryId === storyId,
+	);
+
+	if (storyIndex === -1) {
+		return { success: false, status: 400 };
+	}
+
+	stories.splice(storyIndex, 1);
+	return { success: true, status: 200 };
 };
--- server/src/handlers/user-handlers.js
+++ server/src/handlers/user-handlers.js
@@ -1,3 +1,45 @@
+import { getUser } from './utils.js';
+
+const addFollower = (id, userId, followerId) => {
+	const follower = {
+		id,
+		userId,
+		followerId,
+	};
+
+	user.followers.push(follower);
+};
+
+const addFollowing = (id, userId, followingId) => {
+	const following = {
+		id,
+		userId,
+		followingId,
+	};
+	followerUser.following.push(following);
+};
+
+const removeFromFollowers = (followers, initiatorId) => {
+	const initiatorIndex = followers.findIndex(
+		(follower) => follower.followerId === initiatorId,
+	);
+
+	if (initiatorIndex === -1) {
+		return { success: false };
+	}
+
+	followers.splice(initiatorIndex, 1);
+	return { success: true };
+};
+
+const removeFromFollowing = (followings, targetId) => {
+	const targetIdxInFollowing = followings.findIndex(
+		(following) => following.followingId === targetId,
+	);
+
+	followings.splice(targetIdxInFollowing, 1);
+};
+
 export const getUserFollowers = (userId, users, currentSession) => {
 	const isAuthorizedUser = currentSession.users.includes(userId);
 
@@ -33,4 +75,32 @@ export const getUserStories = (id, session, users) => {
 	const user = users.find((user) => user.id === id);
 
 	return { success: true, user: user.stories, status: 200 };
+};
+
+export const follow = (users, targetId, initiatorId) => {
+	const user = getUser(users, targetId);
+	if (!user) {
+		return { success: false, status: 404 };
+	}
+
+	addFollower(user.followers.length + 1, targetId, initiatorId, users);
+
+	const initiatorUser = getUser(users, initiatorId);
+	addFollowing(initiatorUser.following.length + 1, initiatorId, targetId);
+
+	return { success: true, status: 200 };
+};
+
+export const unfollow = (users, initiatorId, targetId) => {
+	const targetFollowers = getUser(users, targetId).followers;
+	const { success } = removeFromFollowers(targetFollowers, initiatorId);
+
+	if (!success) {
+		return { success, status: 404 };
+	}
+
+	const userFollowingList = getUser(users, initiatorId).following;
+	removeFromFollowing(userFollowingList, targetId);
+
+	return { success: true, status: 200 };
 };
