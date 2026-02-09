4c52c4e47586c9cfcdb05c61c965fa097f4f6da9
Jyothi Praveen Gorre <jyothipraveengorre@Jyothis-MacBook-Pro.local>
2026-02-09 12:51:18 +0530

    [praveen]/ merge

--- server/src/request_handler.js
+++ server/src/request_handler.js
@@ -1,12 +1,3 @@
-import { getRequestHandler } from "./get_handler.js";
-import { postRequestHandler } from "./post_handler.js";
-
-const methods = {
-  GET: getRequestHandler,
-  POST: postRequestHandler,
-};
-
 export const requestHandler = (request) => {
-  const method = request.method;
-  methods[method](request);
+
 };
