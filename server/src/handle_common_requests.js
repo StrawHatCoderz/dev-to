6f7c03e0d37208b3b309b349d639276cded7ac60
Chiranjeevi Gurram <chiranjeevigurram@Chiranjeevis-MacBook-Pro.local>
2026-02-09 16:48:48 +0530

    [chiru][request/ story handler] : implements story handler -create story -delete story

--- server/src/handle_common_requests.js
+++ server/src/handle_common_requests.js
@@ -2,17 +2,16 @@ import { session } from "./current-session.js";
 import { getEveryStory, login, logout } from "./handlers/common_handlers.js";
 import { mockUsers, stories } from "./user/mock-user.js";
 
-export const handleCommonReqests = ({route , body}) => {
-
+export const handleCommonReqests = ({ route, body }) => {
 
   switch (route) {
     case "login": {
-      const username = body[0];
+      const username = body;
       const response = login(username, mockUsers, session);
       return response;
     }
     case "logout": {
-      const userId = body[0];
+      const userId = body;
       const response = logout(userId, session);
       return response;
     }
