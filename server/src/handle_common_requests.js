93ad4e6790b53ad6939efe914adac495170c246a
Chiranjeevi Gurram <chiranjeevigurram@Chiranjeevis-MacBook-Pro.local>
2026-02-09 16:03:55 +0530

    [chiru][request handler/common handlers] : renames params to route , body

--- server/src/handle_common_requests.js
+++ server/src/handle_common_requests.js
@@ -2,17 +2,17 @@ import { session } from "./current-session.js";
 import { getEveryStory, login, logout } from "./handlers/common_handlers.js";
 import { mockUsers, stories } from "./user/mock-user.js";
 
-export const handleCommonReqests = (requestInfo) => {
-  const { command, params } = requestInfo;
+export const handleCommonReqests = ({route , body}) => {
 
-  switch (command) {
+
+  switch (route) {
     case "login": {
-      const username = params[0];
+      const username = body[0];
       const response = login(username, mockUsers, session);
       return response;
     }
     case "logout": {
-      const userId = params[0];
+      const userId = body[0];
       const response = logout(userId, session);
       return response;
     }
