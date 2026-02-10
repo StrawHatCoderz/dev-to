5b1775e4537dc5ce28a280d17d3ff53ca5f0222a
Jyothi Praveen Gorre <jyothipraveengorre@Jyothis-MacBook-Pro.local>
2026-02-10 16:03:39 +0530

    [praveen] merge conflicts

--- server/src/handlers/story_handler2.js
+++ server/src/handlers/story_handler2.js
@@ -1,104 +1,110 @@
-// const getClapIndex = (story, userId) =>
-// 	story.claps.findIndex((clap) => clap.clappedBy === userId);
+import { findStory } from './utils.js';
 
-// const isAlreadyClapped = (story, userId) => {
-// 	return getClapIndex(story, userId) !== -1;
-// };
-
-// const unClapStory = (_id, story, userId, _storyId) => {
-// 	const clapIndex = getClapIndex(story, userId);
-// 	story.claps.splice(clapIndex, 1);
-// };
-
-// const clapStory = (id, story, clappedBy, storyId) => {
-// 	const clap = { id, storyId, clappedBy };
-// 	story.claps.push(clap);
-// };
-
-// const doesStoryExists = (storyId, stories) => {
-// 	return stories.some((story) => story.id === storyId);
-// };
-
-// const addStory = (id, { title, content, authorId, isPublished }, stories) => {
-// 	const story = {
-// 		id,
-// 		authorId,
-// 		title,
-// 		content,
-// 		claps: [],
-// 		isPublished,
-// 		comments: [],
-// 	};
-
-// 	stories.push(story);
-// };
-
-// export const toggleClap = (userId, storyId, stories) => {
-// 	const story = stories.find((story) => story.id === storyId);
-
-// 	if (!story) {
-// 		return { success: false, status: 404 };
-// 	}
-
-// 	const action = isAlreadyClapped(story, userId) ? unClapStory : clapStory;
-// 	const clapId = story.claps.length;
-// 	action(clapId, story, userId, storyId);
-
-// 	return { success: true, status: 200 };
-// };
-
-// export const getComments = (storyId, comments) => {
-// 	const storyComments = comments.filter(
-// 		({ storyId: currentStoryId }) => currentStoryId === storyId,
-// 	);
-
-// 	if (storyComments.length === 0) {
-// 		return { success: false, status: 400, comments: storyComments };
-// 	}
-
-// 	return { success: true, status: 200, comments: storyComments };
-// };
-
-// export const createStory = (storyToCreate, stories) => {
-// 	const { title, content } = storyToCreate;
-// 	const isValidContent = ![content.trim().length, title.trim().length].includes(
-// 		0,
-// 	);
-
-// 	if (!isValidContent) {
-// 		return { success: false, status: 400 };
-// 	}
-
-// 	const id = stories.length + 1;
-// 	addStory(id, storyToCreate, stories);
-
-// 	return { success: true, status: 200 };
-// };
-
-// export const deleteStory = (storyId, stories) => {
-// 	const storyIndex = stories.findIndex(
-// 		({ storyId: currentStoryId }) => currentStoryId === storyId,
-// 	);
-
-// 	if (storyIndex === -1) {
-// 		return { success: false, status: 400 };
-// 	}
-
-// 	stories.splice(storyIndex, 1);
-// 	return { success: true, status: 200 };
-// };
-
-export const retrieveStoryById = (database, id) => {
-  const query = `SELECT * FROM stories WHERE story_id = ?`;
-  const statement = database.prepare(query);
-  return statement.get(id);
+const isAlreadyClapped = (database, userId, storyId) => {
+	const query = `
+		SELECT 1
+		FROM claps
+		WHERE story_id = ? AND clapped_by = ?
+		LIMIT 1
+	`;
+	const statement = database.prepare(query);
+	return statement.get(storyId, userId) !== undefined;
 };
 
-export const getStory = (storyId) => {
-  const story = retrieveStoryById(storyId);
-  if (story === undefined) {
-    return { success: false, status: 404 };
-  }
+const unClapStory = (database, clappedBy, storyId) => {
+	const query = `
+		DELETE FROM claps
+		WHERE story_id = ? AND clapped_by = ?
+	`;
+	const statement = database.prepare(query);
+	return statement.run(storyId, clappedBy);
+};
+
+const clapStory = (database, clappedBy, storyId) => {
+	const query = `
+		INSERT INTO claps(story_id, clapped_by)
+		VALUES (?, ?)
+	`;
+	const statement = database.prepare(query);
+	return statement.run(storyId, clappedBy);
+};
+
+const doesStoryExists = (storyId, stories) => {
+	return stories.some((story) => story.id === storyId);
+};
+
+const addStory = (id, { title, content, authorId, isPublished }, stories) => {
+	const story = {
+		id,
+		authorId,
+		title,
+		content,
+		claps: [],
+		isPublished,
+		comments: [],
+	};
+
+	stories.push(story);
+};
+
+export const toggleClap = (database, userId, storyId) => {
+	const story = findStory(database, storyId);
+
+	if (!story) {
+		return { success: false, status: 404 };
+	}
+
+	const action = isAlreadyClapped(database, userId, storyId)
+		? unClapStory
+		: clapStory;
+	action(database, userId, storyId);
+
+	return { success: true, status: 200 };
+};
+
+export const addComment = (database, userId, storyId, content) => {
+	if (!content) {
+		return { success: false, status: 400 };
+	}
+
+	const story = findStory(database, storyId);
+
+	if (!story) {
+		return { success: false, status: 400 };
+	}
+
+	const query = `
+		INSERT INTO comments(story_id, content, commented_by)
+		VALUES(?, ?, ?)
+	`;
+	const statement = database.prepare(query);
+	const result = statement.run(storyId, content, userId);
+	return { success: true, status: 200, id: result.lastInsertRowid };
+};
+
+export const getComments = (database, storyId) => {
+	const story = findStory(database, storyId);
+
+	if (!story) {
+		return { success: false, status: 400 };
+	}
+
+	const query = `
+		SELECT story_id,content,commented_by,commented_on
+		FROM comments
+		WHERE story_id = ?
+	`;
+	const statement = database.prepare(query);
+	const result = statement.all(storyId);
+
+	return { success: true, status: 200, comments: result };
+};
+
+export const getStory = (storyId, stories) => {
+	const story = retrieveStoryById(storyId);
+	if (!doesStoryExists(storyId, stories)) {
+		return { success: false, status: 404 };
+	}
 
 	return { success: true, status: 200, story };
 };
