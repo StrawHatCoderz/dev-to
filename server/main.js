3852033ca162b5cfa2c0c2aabee200a8509c5789
Umar Khaji <umarkhaji@Umars-MacBook-Pro.local>
2026-02-14 11:52:03 +0530

    [umar] fix passing request argument to handler

--- server/main.js
+++ server/main.js
@@ -1,9 +1,11 @@
 import { DatabaseSync } from 'node:sqlite';
 import { initDB } from './src/db/init.js';
 import { requestHandler } from './src/handlers/request_handler.js';
+
 const database = new DatabaseSync('dev_to.db');
 
-const createRequestHandler = (database) => requestHandler(request, database);
+const createRequestHandler = (database) => (request) =>
+	requestHandler(request, database);
 
 export const main = () => {
 	initDB(database);
