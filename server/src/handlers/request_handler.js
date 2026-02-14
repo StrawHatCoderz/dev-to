171254f0fba22df4d93fb8959fba34abc69b9700
Umar Khaji <umarkhaji@Umars-MacBook-Pro.local>
2026-02-14 11:50:13 +0530

    [umar] adds fallback route for invalid routes and add logger

--- server/src/handlers/request_handler.js
+++ server/src/handlers/request_handler.js
@@ -1,6 +1,7 @@
 import { commonRequestRouter } from '../routes/common_requests_route.js';
-import { handleStoryRequests } from '../routes/story_requests_route.js';
+import { storyRequestRouter } from '../routes/story_requests_route.js';
 import { handleUserRequest } from '../routes/user_requests_route.js';
+import { logRequest } from '../utils.js';
 
 const isCommonRoute = (route) => {
 	const commonRoutes = ['login', 'logout', 'stories'];
@@ -19,7 +20,7 @@ const parseCommonRequest = async (request) => {
 const parseRequest = async (request) => {
 	const pathname = new URL(request.url).pathname;
 	const { method } = request;
-	const [_, _, route, ...params] = pathname.split('/');
+	const [_, __, route, ...params] = pathname.split('/');
 	return method === 'POST'
 		? { route, params: [], body: await request.json() }
 		: { route, body: {}, params };
@@ -29,14 +30,24 @@ export const requestHandler = async (request, database) => {
 	const pathname = new URL(request.url).pathname;
 	const [_, rootRoute] = pathname.split('/');
 
+	logRequest(request.method, pathname);
 	if (isCommonRoute(rootRoute)) {
 		const requestInfo = await parseCommonRequest(request);
-		return await commonRequestRouter(requestInfo, database);
-	} else if (rootRoute === 'story') {
-		const requestInfo = await parseRequest(request);
-		return await handleStoryRequests(requestInfo);
-	} else if (rootRoute === 'user') {
-		const requestInfo = await parseRequest(request);
-		return await handleUserRequest(requestInfo);
+		return commonRequestRouter(requestInfo, database);
 	}
+
+	if (rootRoute === 'story') {
+		const requestInfo = await parseRequest(request);
+		return storyRequestRouter(requestInfo);
+	}
+
+	if (rootRoute === 'user') {
+		const requestInfo = await parseRequest(request);
+		return handleUserRequest(requestInfo);
+	}
+
+	return new Response(JSON.stringify({ error: 'Not Found' }), {
+		status: 404,
+		headers: { 'content-type': 'application/json' },
+	});
 };
