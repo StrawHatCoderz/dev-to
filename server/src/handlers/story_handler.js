138eb80e3b3c28e9277613cdd00ef5444a1ad3e7
Umar Khaji <umarkhaji@Umars-MacBook-Pro.local>
2026-02-14 11:16:06 +0530

    [umar] moves all queries into a seprate file

--- server/src/handlers/story_handler.js
+++ server/src/handlers/story_handler.js
@@ -1,36 +1,35 @@
+import {
+	addClapQuery,
+	addCommentQuery,
+	addIntoPublishedStoriesQuery,
+	addStoryQuery,
+	deleteClapQuery,
+	deleteStoryQuery,
+	getClapStatusQuery,
+	getCommentQuery,
+} from '../db/queries.js';
 import { findStory, findUser } from '../utils.js';
 
 const isAlreadyClapped = (database, userId, storyId) => {
-	const query = `
-		SELECT 1
-		FROM claps
-		WHERE story_id = ? AND clapped_by = ?
-		LIMIT 1
-	`;
+	const query = getClapStatusQuery();
 	const statement = database.prepare(query);
 	return statement.get(storyId, userId) !== undefined;
 };
 
 const unClapStory = (database, clappedBy, storyId) => {
-	const query = `
-		DELETE FROM claps
-		WHERE story_id = ? AND clapped_by = ?
-	`;
+	const query = deleteClapQuery();
 	const statement = database.prepare(query);
 	return statement.run(storyId, clappedBy);
 };
 
 const clapStory = (database, clappedBy, storyId) => {
-	const query = `
-		INSERT INTO claps(story_id, clapped_by)
-		VALUES (?, ?)
-	`;
+	const query = addClapQuery();
 	const statement = database.prepare(query);
 	return statement.run(storyId, clappedBy);
 };
 
 const removeRecord = (database, storyId) => {
-	const query = `DELETE FROM stories where story_id = ?`;
+	const query = deleteStoryQuery();
 	const statement = database.prepare(query);
 	return statement.run(storyId);
 };
@@ -38,7 +37,7 @@ const removeRecord = (database, storyId) => {
 const insertIntoStories = (database, storyToCreate) => {
 	const { title, content, authorId, isPublished } = storyToCreate;
 
-	const query = `INSERT INTO stories(title, content, author_id, is_published) VALUES (?,?,?,?)`;
+	const query = addStoryQuery();
 	const statement = database.prepare(query);
 
 	return statement.run(title, content, authorId, isPublished);
@@ -56,7 +55,7 @@ export const deleteStory = (database, storyId) => {
 };
 
 const insertIntoPublished = (database, storyId) => {
-	const query = `INSERT INTO published_stories(story_id) VALUES (?)`;
+	const query = addIntoPublishedStoriesQuery();
 	const statement = database.prepare(query);
 	return statement.run(storyId);
 };
@@ -107,10 +106,7 @@ export const addComment = (database, userId, storyId, content) => {
 		return { success: false, status: 400 };
 	}
 
-	const query = `
-		INSERT INTO comments(story_id, content, commented_by)
-		VALUES(?, ?, ?)
-	`;
+	const query = addCommentQuery();
 
 	const statement = database.prepare(query);
 	const result = statement.run(storyId, content, userId);
@@ -125,11 +121,7 @@ export const getComments = (database, storyId) => {
 		return { success: false, status: 400 };
 	}
 
-	const query = `
-		SELECT story_id,content,commented_by,commented_on
-		FROM comments
-		WHERE story_id = ?
-	`;
+	const query = getCommentQuery();
 
 	const statement = database.prepare(query);
 	const result = statement.all(storyId);
--- server/src/handlers/utils.js
+++ server/src/handlers/utils.js
@@ -1,47 +1,43 @@
-export const findUser = (users, target, findByLabel = 'id') => {
-	return users.find((user) => user[findByLabel] === target);
-};
+import {
+	addUserToSessionQuery,
+	findStoryQuery,
+	findUserQuery,
+	getStoriesQuery,
+	isAuthorizedQuery,
+} from '../db/queries.js';
 
-export const findUserInDB = (database, target, findByColumn = 'id') => {
-	const query = `SELECT id, username FROM user WHERE ? = ?`;
+export const findUser = (database, target, findByColumn = 'id') => {
+	const query = findUserQuery(findByColumn);
 	const statement = database.prepare(query);
 	return statement.get(findByColumn, target);
 };
 
 export const addUserToSession = (database, userId) => {
-	const query = `INSERT INTO session(user_id) VALUES(?)`;
+	const query = addUserToSessionQuery();
 	const statement = database.prepare(query);
 	return statement.run(userId);
 };
 
-export const isAuthorizedInDB = (database, userId) => {
-	const query = `SELECT 1 FROM session WHERE user_id = ? LIMIT 1`;
+export const isAuthorized = (database, userId) => {
+	const query = isAuthorizedQuery();
 	const statement = database.prepare(query);
 	return statement.get(userId) !== undefined;
 };
 
 export const retrieveStories = (database) => {
-	const query = `SELECT * FROM stories`;
+	const query = getStoriesQuery();
 	const statement = database.prepare(query);
 	return statement.all();
 };
 
 export const removeUserFromSession = (database, userId) => {
-	const query = `DELETE FROM session WHERE user_id = ?`;
+	const query = removeUserFromSessionQuery();
 	const statement = database.prepare(query);
 	return statement.run(userId);
 };
 
-export const isAuthorized = (userId, session) => {
-	return session.users.includes(userId);
-};
-
 export const findStory = (database, storyId) => {
-	const query = `
-		SELECT story_id, title, content, author_id, is_published, created_on, updated_on
-		FROM stories
-		WHERE story_id = ?
-	`;
+	const query = findStoryQuery();
 	const statement = database.prepare(query);
 
 	return statement.get(storyId);
