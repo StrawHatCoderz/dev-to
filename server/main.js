2b65eada565e9b4d1d31aa95a9e2608036c03089
Umar Khaji <umarkhaji@Umars-MacBook-Pro.local>
2026-02-14 12:10:35 +0530

    server/src/db/init.js

--- server/main.js
+++ server/main.js
@@ -1,15 +1,13 @@
 import { DatabaseSync } from 'node:sqlite';
-import { initDB } from './src/db/init.js';
 import { requestHandler } from './src/handlers/request_handler.js';
 
-const database = new DatabaseSync('dev_to.db');
-
-const createRequestHandler = (database) => (request) =>
-	requestHandler(request, database);
+const createRequestHandler = () => {
+	const database = new DatabaseSync('dev_to.db');
+	return (request) => requestHandler(request, database);
+};
 
 export const main = () => {
-	initDB(database);
-	Deno.serve(createRequestHandler(database));
+	Deno.serve(createRequestHandler());
 };
 
 main();
